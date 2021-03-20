37d64413ec25bdbbd35bec4fb89496edbb8e0d78
JimmyAllDay <jameswhmarshall@gmail.com>
2021-03-20 13:09:59 +1100

    debugged node environment setup no 2

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // Setup empty JS object to act as endpoint for all routes
+const routesEndPoint = {};
 // Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
-
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static("Project_3_Weather_App"));
+
+const port = 8000;
 // Spin up the server
+const server = app.listen(port, listening);
 
 // Callback to debug
+function listening() {
+  console.log("server running");
+  console.log(`running on local host: ${port}`);
+}
 
 // Initialize all route with a callback function
 
